ce8e3cbc7e0366084d3501d2cfef21e84982ca7f
ALEXANDRA-MARIA ISTRATE <alexandra.maria.istrate@stud.ubbcluj.ro>
2021-11-28 23:33:22 +0200

    Add project files.

---
+++ Data/Istrate_Alexandra_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Istrate_Alexandra_Lab8.Models;
+
+namespace Istrate_Alexandra_Lab8.Data
+{
+    public class Istrate_Alexandra_Lab8Context : DbContext
+    {
+        public Istrate_Alexandra_Lab8Context (DbContextOptions<Istrate_Alexandra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Istrate_Alexandra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Istrate_Alexandra_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
