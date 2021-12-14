568907955c13b804dd4023e6c13c36b79d008721
ALEXANDRA-MARIA ISTRATE <alexandra.maria.istrate@stud.ubbcluj.ro>
2021-12-14 23:17:44 +0200

    Lab9

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,8 +10,10 @@ namespace Istrate_Alexandra_Lab8.Models
     public class Book
     {
         public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
         public string Author { get; set; }
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
--- Models/BookCategoriesPageModel.cs
+++ Models/BookCategoriesPageModel.cs
@@ -10,10 +10,13 @@ namespace Istrate_Alexandra_Lab8.Models
     public class BookCategoriesPageModel : PageModel
     {
         public List<AssignedCategoryData> AssignedCategoryDataList;
-        public void PopulateAssignedCategoryData(Istrate_Alexandra_Lab8Context context, Book book)
+        public void PopulateAssignedCategoryData(Istrate_Alexandra_Lab8Context context,
+        Book book)
         {
             var allCategories = context.Category;
-            var bookCategories = new HashSet<int>(book.BookCategories.Select(c => c.CategoryID)); // AssignedCategoryDataList = new List<AssignedCategoryData>();
+            var bookCategories = new HashSet<int>(
+            book.BookCategories.Select(c => c.CategoryID)); //
+            AssignedCategoryDataList = new List<AssignedCategoryData>();
             foreach (var cat in allCategories)
             {
                 AssignedCategoryDataList.Add(new AssignedCategoryData
@@ -32,21 +35,30 @@ namespace Istrate_Alexandra_Lab8.Models
                 return;
             }
             var selectedCategoriesHS = new HashSet<string>(selectedCategories);
-            var bookCategories = new HashSet<int>(bookToUpdate.BookCategories.Select(c => c.Category.ID));
+            var bookCategories = new HashSet<int>
+            (bookToUpdate.BookCategories.Select(c => c.Category.ID));
             foreach (var cat in context.Category)
             {
                 if (selectedCategoriesHS.Contains(cat.ID.ToString()))
                 {
                     if (!bookCategories.Contains(cat.ID))
                     {
-                        bookToUpdate.BookCategories.Add(new BookCategory { BookID = bookToUpdate.ID, CategoryID = cat.ID });
+                        bookToUpdate.BookCategories.Add(
+                        new BookCategory
+                        {
+                            BookID = bookToUpdate.ID,
+                            CategoryID = cat.ID
+                        });
                     }
                 }
                 else
                 {
                     if (bookCategories.Contains(cat.ID))
                     {
-                        BookCategory courseToRemove = bookToUpdate.BookCategories.SingleOrDefault(i => i.CategoryID == cat.ID);
+                        BookCategory courseToRemove
+                        = bookToUpdate
+                        .BookCategories
+                       .SingleOrDefault(i => i.CategoryID == cat.ID);
                         context.Remove(courseToRemove);
                     }
                 }
